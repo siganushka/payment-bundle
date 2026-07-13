910c7437592a67369849203f6d7aef74936b60c2
siganushka <siganushka@gmail.com>
2026-07-13 17:40:40 +0800

    refactor: remove unused namespace

--- src/Gateway/WxpayNative.php
+++ src/Gateway/WxpayNative.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Siganushka\PaymentBundle\Gateway;
 
 use Siganushka\PaymentBundle\Entity\Payment;
-use Symfony\Component\HttpFoundation\Response;
 
 class WxpayNative extends AbstractWxpay
 {
